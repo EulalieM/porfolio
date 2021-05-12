3d558fd4283ab2e02d8e8ed0d0d19fce01b8982e
Eulalie Moreau <eulalie.m16000@gmail.com>
2021-05-12 17:47:24 +0200

    Update : page à propos

--- apropos.php
+++ apropos.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Primary Meta Tags -->
-    <title>Eulalie Moreau | Design & Développement web - Compétences</title>
+    <title>Eulalie Moreau | Design & Développement web - À propos</title>
     <meta name="title" content="Eulalie Moreau | Design & Développement web - Compétences">
     <meta name="description" content="Mes compétences en terme d'analyse de projet, de conception et design de sites, développement et qualité web.">
 
@@ -30,7 +30,7 @@
     <link rel="stylesheet" href="styles/styles.css">
     <link rel="stylesheet" href="styles/styles-header.css">
     <link rel="stylesheet" href="styles/styles-footer.css">
-    <link rel="stylesheet" href="styles/styles-competences.css">
+    <link rel="stylesheet" href="styles/styles-apropos.css">
     <link rel="shortcut icon" href="img/favicon.ico">
 </head>
 
@@ -44,98 +44,117 @@
 
     <main>
         <div>
-            <h1>Mes compétences</h1>
-            <section class="competences">
-                <h2>Étude de marché</h2>
-                    <div class="com-cards">
-                        <div class="com-card">
-                            <img src="img/besoins.svg" alt="">
-                            <p>Définir les besoins</p>                           
-                        </div>
-                        <div class="com-card">
-                            <img src="img/cible.svg" alt="">
-                            <p>Déterminer la cible du projet</p>                           
-                        </div>
-                    </div>
-                    <div class="com-cards">
-                        <div class="com-card">
-                            <img src="img/conccurence.svg" alt="">
-                            <p>Étudier la conccurence</p>                            
-                        </div>
-                        <div class="com-card">
-                            <img src="img/benchmarking.svg" alt="">
-                            <p>Réaliser un benchmarking</p>                           
-                        </div>
-                    </div>
+            <h1>À propos</h1>
+            <section class="pro-section">
+                <h2>Qui suis-je ?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pharetra eros eu maximus interdum. In malesuada placerat orci, et convallis lorem. Nullam pretium nibh vel augue porttitor, sed scelerisque metus tincidunt. Ut convallis sapien a dolor pharetra, suscipit dictum neque lobortis. Fusce non eleifend velit, in dapibus neque.</p>
+                <a href="cv_eulalie_moreau.pdf" target="_blank" class="cta">Voir mon CV</a>
             </section>
-            <section class="competences">
-                <h2>Conception et design graphique</h2>
-                <div class="com-cards">                   
-                    <div class="com-card">
-                        <img src="img/arborescence.svg" alt="">
-                        <p>Construire une arborescence</p>                        
-                    </div>
-                    <div class="com-card">
-                        <img src="img/charte.svg" alt="">
-                        <p>Élaborer une charte graphique</p>                       
-                    </div>
+            <section class="pro-section">
+                <h2>Mes compétences</h2>
+                <div class="pro-content">
+                    <img src="img/arborescence.svg" alt="" class="pro-img-left">
+                    <div>
+                        <h3>Conception de site web</h3>
+                        <ul>
+                            <li>Préciser les <strong>besoins</strong> du projet</li>
+                            <li>Déterminer la <strong>cible</strong></li>
+                            <li>Étudier la <strong>conccurence</strong></li>
+                            <li>Réaliser un <strong>benchmarking</strong></li>
+                            <li>Définir l'<strong>arborescence</strong> et l'<strong>ergonomie</strong> du site</li>
+                        </ul> 
+                    </div>                      
                 </div>
-                <div class="com-cards">
-                    <div class="com-card">
-                        <img src="img/maquette.svg" alt="">
-                        <p>Designer une maquette graphique</p>                        
+                <div class="pro-content">
+                    <div>
+                        <h3>Maquettage et design graphique d'interface</h3> 
+                        <ul>
+                            <li>Réaliser la maquette fonctionnelle</li>
+                            <li>Réaliser la maquette graphique : <strong>Adobe Xd</strong>, <strong>Figma</strong></li>
+                            <li>Traiter les images : <strong>Gimp</strong>, <strong>Illustrator</strong></li>
+                            <li>Réaliser un <strong>prototype</strong></li>
+                        </ul>   
                     </div>
-                    <div class="com-card">
-                        <img src="img/logiciels.svg" alt="">
-                        <p>Logiciels : Adobe Xd, Figma, Gimp, Illustrator...</p>                        
-                    </div>                   
+                    <img src="img/maquette.svg" alt="" class="pro-img-right">
                 </div>
+                <div class="pro-content">
+                    <img src="img/front.svg" alt="" class="pro-img-left">
+                    <div>
+                        <h3>Intégration et développement</h3>
+                        <ul>
+                            <li>Intégrer le site à l'aide de CMS : <strong>WordPress</strong></li>
+                            <li>Intégrer la maquette graphique sur-mesure: <strong>HTML</strong>, <strong>CSS</strong>, <strong>JS</strong></li>
+                            <li>Développer la partie back-end : <strong>PHP</strong></li>
+                            <li>Adapter le site en version <strong>responsive design</strong></li>
+                        </ul>   
+                    </div>                       
+                </div>
+                <div class="pro-content">
+                    <div>
+                        <h3>Qualité web et expérience utilisateur</h3>
+                        <ul>
+                            <li>Analyser les <strong>performances</strong></li>
+                            <li><strong>Sécuriser</strong> le site</li>
+                            <li>Vérifier l'<strong>accessibilité</strong></li>
+                            <li>Optimiser pour les <strong>moteurs de recherche</strong></li>
+                        </ul>  
+                    </div>
+                    <img src="img/front.svg" alt="" class="pro-img-right">
+                </div>      
             </section>
-            <section class="competences">
-                <h2>Intégration et développement web</h2>
-                <div class="com-cards">
-                    <div class="com-card">
-                        <img src="img/front.svg" alt="">
-                        <p>Intégration front-end : HTML, CSS, JavaScript</p>                       
-                    </div>
-                    <div class="com-card">
-                        <img src="img/back.svg" alt="">
-                        <p>Développement back-end : PHP</p>                      
+            <section class="pro-section">
+                <h2>Mon parcours</h2>
+                
+                <div class="container">
+                    <div id="timeline">
+                        <div class="timeline-item">
+                            <div class="timeline-icon">
+                                <!-- icone -->
+                            </div>
+                            <div class="timeline-content">
+                                <h2>2012-2016</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                                Atque, facilis quo maiores magnam modi ab libero praesentium blanditiis.</p>
+                                <a href="#" class="cta">button</a>
+                            </div>
+                        </div>
+
+                        <div class="timeline-item">
+                            <div class="timeline-icon">
+                                <!-- icone -->
+                            </div>
+                            <div class="timeline-content right">
+                                <h2>2016-2017</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, facilis quo. Maiores magnam modi ab libero praesentium blanditiis consequatur aspernatur accusantium maxime molestiae sunt ipsa.</p>
+                                <a href="#" class="cta">button</a>
+                            </div>
+                        </div>
+
+                        <div class="timeline-item">
+                            <div class="timeline-icon">
+                                <!-- icone -->
+                            </div>
+                            <div class="timeline-content">
+                                <h2>2017-2019</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, facilis quo. Maiores magnam modi ab libero praesentium blanditiis consequatur aspernatur accusantium maxime molestiae sunt ipsa.</p>
+                                <a href="#" class="cta">button</a>
+                            </div>
+                        </div>
+
+                        <div class="timeline-item">
+                            <div class="timeline-icon">
+                                <!-- icone -->
+                            </div>
+                            <div class="timeline-content right">
+                                <h2>2020</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, facilis quo. Maiores magnam modi ab libero praesentium blanditiis consequatur aspernatur accusantium maxime molestiae sunt ipsa.</p>
+                                <a href="#" class="cta">button</a>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
-                <div class="com-cards">
-                    <div class="com-card">
-                        <img src="img/wordpress.svg" alt="">
-                        <p>Création de site à l'aide de CMS : WordPress</p>                       
-                    </div>
-                    <div class="com-card">
-                        <img src="img/refonte.svg" alt="">
-                        <p>Refonte de site pré-existant</p>                       
-                    </div>
-                </div>    
-            </section>
-            <section class="competences">
-                <h2>Expérience utilisateur et qualité web</h2>
-                <div class="com-cards">
-                    <div class="com-card">
-                        <img src="img/qualite.svg" alt="">
-                        <p>Audit qualité : Opquast, RGAA</p>                       
-                    </div>
-                    <div class="com-card">
-                        <img src="img/securite.svg" alt="">
-                        <p>Sécurité du site : OWASP</p>                        
-                    </div>
-                </div>
-                <div class="com-cards">
-                    <div class="com-card">
-                        <img src="img/ergo.svg" alt="">
-                        <p>Navigation et ergonomie</p>                        
-                    </div>
-                    <div class="com-card">
-                        <img src="img/seo.svg" alt="">
-                        <p>Optimisation pour les moteurs de recherche</p>                      
-                    </div>
-                </div>
+
             </section>
         </div>
     </main>
