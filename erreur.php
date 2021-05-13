6edb441113d4a1ea1559a1913d73ca2b886c4e9c
Eulalie Moreau <eulalie.m16000@gmail.com>
2021-05-13 16:35:03 +0200

    Error 404

---
+++ erreur.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Eulalie Moreau | Design & Développement web - Erreur 404</title>
+    <meta name="title" content="Eulalie Moreau | Design &
+    Développement web - Erreur 404">
+    
+    <link href="https://fonts.googleapis.com/css2?family=El+Messiri:wght@400;500;600;700&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500;1,600&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/normalize.css">
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/styles-header.css">
+    <link rel="stylesheet" href="styles/styles-footer.css">
+    <link rel="stylesheet" href="styles/styles-404.css">
+    <link rel="shortcut icon" href="img/favicon.ico">
+</head>
+
+<body class="pre-body">
+
+    <header>
+        <?php include('includes/header.php'); ?>
+    </header>
+
+    <?php include('includes/burger-menu.php'); ?>
+
+    <main class="404-main">
+       <p>Erreur 404 - Page non trouvé :-(</p> 
+    </main>
+
+    <footer>
+        <?php include('includes/footer.php'); ?>
+    </footer>
+
+    <script src="https://kit.fontawesome.com/b6fad4163c.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="scripts/script.js"></script>
+
+</body>
+
+</html>
