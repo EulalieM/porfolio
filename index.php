311addeb78432bbfaf2eb518c0d36cc1ef43b8fe
Eulalie Moreau <eulalie.m16000@gmail.com>
2021-05-17 22:43:02 +0200

    Update

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
                 <h2>Design &<br>
                 Développement web</h2>
                 <p class="pre-description">Persévérante, passionnée et rigoureuse, je poursuis mes études en alternance et j'intègre un Bachelor développeur web en septembre 2021.</p>
-                <a href="contact" class="pre-contact cta">Me contacter</a>
+                <a href="contact" class="pre-contact cta">contacter</a>
             </div>
             <div class="pre-content">
                 <img src="img/eulalie_photo.png" alt="Photo Eulalie Moreau" class="pre-photo">
