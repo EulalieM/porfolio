320b4d0b125a72fa22fe01a79f574e2426883ca1
Eulalie Moreau <eulalie.m16000@gmail.com>
2021-05-20 13:16:53 +0200

    Update link index

--- includes/burger-menu.php
+++ includes/burger-menu.php
@@ -1,7 +1,7 @@
 <div id="h-mobile">
     <nav>
         <ul>
-            <li><a href="index">Présentation</a></li>
+            <li><a href="/">Présentation</a></li>
             <li><a href="realisations">Réalisations</a></li>
             <li><a href="contact">Contact</a></li>
         </ul>
--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
 <div class="h-header" id="h-desktop">
-    <a href="index" class="h-logo"><img src="img/eulalie_photo_mini.png" alt=""></a>
+    <a href="/" class="h-logo"><img src="img/eulalie_photo_mini.png" alt=""></a>
     <button id="icone-burger"><i id="icon" class="fas fa-bars"></i></button>
     <nav class="h-nav">
         <ul>
-            <li><a href="index">Présentation</a></li>
+            <li><a href="/">Présentation</a></li>
             <li><a href="realisations">Réalisations</a></li>
             <li><a href="contact">Contact</a></li>
         </ul>
